d292f1776063ee065279f2027be4039694b52062
bcroman <126107298+bcroman@users.noreply.github.com>
2024-01-18 14:13:44 +0000

    Add files via upload
    Version 1b

--- Pontoon/src/pontoon/Pontoon.java
+++ Pontoon/src/pontoon/Pontoon.java
@@ -4,46 +4,85 @@ import java.util.Scanner;
 
 public class Pontoon {
 
+    //Methods
+    static int getRandomNum() {
+        int x; //varble
+        x = (int) (Math.random() * 10) + 1; //radom between 1 and 10
+        return x; //reture valur
+    }
+
+    static int calcTotal(int x, int y) {
+        int z;
+        z = x + y;
+        return z;
+    }
+
+    static boolean getResult(int z) {
+        boolean result;
+
+        //If Statement
+        if (z > 19 && z <= 21) {
+            result = true;
+        } else {
+            result = false;
+        }
+        return result;
+    }
+
+    static boolean busted(int z) {
+        boolean busted = false;
+        if (z > 21) {
+            busted = true;
+        }
+        return busted;
+    }
+
+    //Main Code
     public static void main(String[] args) {
         Scanner kboard = new Scanner(System.in);
-        
+
         //Variables
         String choice = "";
         int card1 = 0;
         int card2 = 0;
         int newCard = 0;
         int total = 0;
-        
+
         //Generate Random Numbers
-        card1 = (int)(Math.random()*10)+1;
-        card2 = (int)(Math.random()*10)+1;
-        
+        card1 = getRandomNum();
+        card2 = getRandomNum();
+
         //Add Total
-        total = card1+card2;
-        
+        total = calcTotal(card1, card2);
+
         //Diaplay
-        System.out.println("You have drawn "+card1+" and "+card2);
+        System.out.println("You have drawn " + card1 + " and " + card2);
         System.out.println("Do you wish to draw another card y/n?");
-        
+
         //User Input
         choice = kboard.next();
-          
+
         //Whlie Loop
-        while(choice.equalsIgnoreCase("y")){
-            newCard = (int)(Math.random()*10)+1;
-            System.out.println("You have drawn "+newCard);
-            total = total+newCard;
+        while (choice.equalsIgnoreCase("y")) {
+            newCard = getRandomNum();
+            System.out.println("You have drawn " + newCard);
+            total = total + newCard;
             System.out.println("Do you wish to draw another card y/n?");
             choice = kboard.next();
         }
-        
+
+        //call getResult method
+        boolean result = getResult(total);
+        //call busted method
+        boolean bustedOver21 = busted(total);
+
         //If Statement
-        if(total > 19 && total <=21){
+        if (result == true && bustedOver21 == false) {
             System.out.println("You have won");
-        }else{
+        } else {
             System.out.println("You have lost");
         }
-        
-        System.out.println("You have total of "+total);
+
+        System.out.println("You have total of " + total);
     }
 }
